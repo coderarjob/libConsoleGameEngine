c3100d4af26456c68f6ca08d65f7213c67e47c4b
Arjob Mukherjee <mukherjeearjob@gmail.com>
2022-09-01 09:16:31 +0530

    Mostly code refactoring changes.

--- examples/life.rs
+++ examples/life.rs
@@ -20,11 +20,10 @@ struct Game {
 
 impl GamePlay for Game {
     fn draw(&mut self, engine: &mut GameEngine, elapsed_time: f64) -> bool {
-        let snapshot = self.world.clone();
-
         // Draw the Generation and FPS
         let fps = 1.0 / elapsed_time;
         let legend = format!("Generation: {} FPS: {:3.2}", self.generation, fps);
+
         engine.draw_string(
             0,
             0,
@@ -38,39 +37,36 @@ impl GamePlay for Game {
             return false;
         }
 
+        // Game logic and drawing
+        let snapshot = self.clone();
+
         for x in 1..engine.width() - 1 {
             for y in 1..engine.height() - 1 {
-                if self.world[y * self.width + x] == 1 {
-                    engine.draw_pixel(
-                        x,
-                        y,
-                        BlockChars::Solid,
-                        BackgroundColors::Black,
-                        ForegroundColors::Green,
-                    )
-                } else {
-                    engine.draw_pixel(
-                        x,
-                        y,
-                        BlockChars::Blank,
-                        BackgroundColors::Black,
-                        ForegroundColors::White,
-                    )
+                let mut block_char = BlockChars::Solid;
+                let mut bg_color = BackgroundColors::Black;
+                let mut fg_color = ForegroundColors::Green;
+
+                if self.get_cell(x, y) == 0 {
+                    block_char = BlockChars::LightShaded;
+                    bg_color = BackgroundColors::Black;
+                    fg_color = ForegroundColors::White;
                 }
 
-                let cell = |x: usize, y: usize| snapshot[y * self.width + x];
+                engine.draw_pixel(x, y, block_char, bg_color, fg_color);
+
+                let cell = |x: usize, y: usize| snapshot.get_cell(x, y);
                 let live_neighbours = cell(x - 1, y - 1) + cell(x, y - 1) + cell(x + 1, y - 1) +
                                       cell(x - 1, y) + 0 + cell(x + 1, y) +
                                       cell(x - 1, y + 1) + cell(x, y + 1) + cell(x + 1, y + 1);
 
                 let is_alive = cell(x, y) == 1;
-
-                self.world[y * self.width + x] = match live_neighbours {
-                    2 => if is_alive { 1 } else { 0 },
-                    3 => 1,
-                    0..=1 => 0,
-                    4.. => 0,
+                let new_value = match live_neighbours {
+                    2 => if is_alive { true } else { false },
+                    3 => true,
+                    0..=1 => false,
+                    4.. => false,
                 };
+                self.set_cell(x, y, new_value);
 
                 /*self.world[y * self.width + x] = match live_neighbours {
                     2..=2 => 1, // Reproducing
@@ -81,6 +77,7 @@ impl GamePlay for Game {
         }
 
         self.generation += 1;
+        //std::thread::sleep(std::time::Duration::from_millis(500));
         true
     }
 }
@@ -94,7 +91,16 @@ impl Game {
             generation: 0,
         }
     }
-    pub fn cell_pattern(&mut self, x: usize, y: usize, pattern: &str) {
+
+    pub fn get_cell(&self, x: usize, y: usize) -> u8 {
+        self.world[y * self.width + x]
+    }
+
+    pub fn set_cell(&mut self, x: usize, y: usize, is_alive: bool) {
+        self.world[y * self.width + x] = is_alive as u8;
+    }
+
+    pub fn fill_pattern(&mut self, x: usize, y: usize, pattern: &str) {
         let mut x = x;
 
         for c in pattern.as_bytes() {
@@ -106,11 +112,12 @@ impl Game {
 
 fn main() {
     let mut game = Game::new(220, 120);
-    game.cell_pattern(1, 1, ".0.");
-    game.cell_pattern(1, 2, "..0");
-    game.cell_pattern(1, 3, "000");
+    game.fill_pattern(1, 1, ".0.");
+    game.fill_pattern(1, 2, "..0");
+    game.fill_pattern(1, 3, "000");
 
-    game.cell_pattern(55, 60, "00000000.00000...000......0000000.00000");
+    game.fill_pattern(55, 60, "00000000.00000...000......0000000.00000");
+    game.fill_pattern(55, 2, "00000000.00000...000......0000000.00000");
 
     let mut engine = GameEngine::new(game.width, game.height);
     engine.begin(&mut game).unwrap();
